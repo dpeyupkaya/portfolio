333b11b64c884ae36ce9a0dc621f4e7ea7776dab
Harun Dursun <harundursun935@gmail.com>
2025-01-27 03:34:03 +0300

    New pages added: Contact, About Me and Homepage. Navbar has been updated. AOS and Framer Motion integration was made. Added CSS animations.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-import { Navbar } from './components/Navbar'
+import React, { useEffect } from 'react';
+import { Navbar } from './components/Navbar';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
-export const App = () => {
+const App = () => {
+  useEffect(() => {
+    AOS.init({
+      duration:1000,
+      once: false,
+      mirror: true
+    });
+  }, []);
+
   return (
-    <div><Navbar /></div>
-  )
-}
+    <div className="scroll-smooth">
+      <Navbar />
+      <Home />
+      <About />
+      <Contact />
+    </div>
+  );
+};
+
 export default App;
---
+++ client/src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+// pages/Contact.js
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <section id="contact" className="min-h-screen pt-16 flex items-center justify-center bg-red-50">
+      <h1 className="text-4xl font-bold text-red-600">İletişim Sayfası</h1>
+    </section>
+  );
+}
